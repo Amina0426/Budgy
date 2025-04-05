5c58f791ad1b720694ee92bac7d681cca0e835c4
Amina0426 <amublue26@gmail.com>
2025-04-05 17:00:41 +0530

    impUpdate

--- service-worker.js
+++ service-worker.js
@@ -10,12 +10,18 @@ self.addEventListener("install", event => {
             ]);
         })
     );
+    self.skipWaiting();
 });
+
+self.addEventListener("activate",event=>{
+    self.clients.claim();
+})
 
 self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request).then(response => {
-            return response || fetch(event.request);
+            return response || fetch(event.request).catch(()=>
+            caches.match("./index.html"));
         })
     );
 });
