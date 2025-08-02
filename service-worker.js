25af753253a6fa7a59c51281ffc8e9d7fa165783
Amina0426 <amublue26@gmail.com>
2025-08-02 19:17:14 +0530

    v4.7

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "expense-cache-v4.6";
+const CACHE_NAME = "expense-cache-v4.7";
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
